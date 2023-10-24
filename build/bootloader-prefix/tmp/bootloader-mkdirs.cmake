df8df1041e570653a74cef25185aa295b8d1acad
EddieSherban <109121401+EddieSherban@users.noreply.github.com>
2023-10-24 13:41:24 -0700

    Initial commit: Setting up uC with LED toggle

---
+++ build/bootloader-prefix/tmp/bootloader-mkdirs.cmake
@@ -0,0 +1,22 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "D:/esp/esp-idf/components/bootloader/subproject"
+  "D:/Documents/Electrical Egneinerineign/Electrical Engineering/Projects/test/eddies_projet/build/bootloader"
+  "D:/Documents/Electrical Egneinerineign/Electrical Engineering/Projects/test/eddies_projet/build/bootloader-prefix"
+  "D:/Documents/Electrical Egneinerineign/Electrical Engineering/Projects/test/eddies_projet/build/bootloader-prefix/tmp"
+  "D:/Documents/Electrical Egneinerineign/Electrical Engineering/Projects/test/eddies_projet/build/bootloader-prefix/src/bootloader-stamp"
+  "D:/Documents/Electrical Egneinerineign/Electrical Engineering/Projects/test/eddies_projet/build/bootloader-prefix/src"
+  "D:/Documents/Electrical Egneinerineign/Electrical Engineering/Projects/test/eddies_projet/build/bootloader-prefix/src/bootloader-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "D:/Documents/Electrical Egneinerineign/Electrical Engineering/Projects/test/eddies_projet/build/bootloader-prefix/src/bootloader-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "D:/Documents/Electrical Egneinerineign/Electrical Engineering/Projects/test/eddies_projet/build/bootloader-prefix/src/bootloader-stamp${cfgdir}") # cfgdir has leading slash
+endif()
