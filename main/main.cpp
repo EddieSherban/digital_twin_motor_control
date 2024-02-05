9497152367405a470612c08de20ce5ff64606c07
simonh-nguyen <nguyenh.simon@gmail.com>
2024-02-05 09:15:56 -0800

    Added .gitignore file

--- main/main.cpp
+++ main/main.cpp
@@ -3,13 +3,14 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_log.h"
+#include "motor_control.h"
 
 extern "C" void app_main(void)
 {
     while (1)
     {
-        ESP_LOGI("Main", "Simon is cool.");
-        ESP_LOGE("Main", "Eddie is cooler");
+        ESP_LOGI("Main", "Test 1");
+        ESP_LOGE("Main", "Test 2");
         vTaskDelay(500/portTICK_PERIOD_MS);
     }
 }
---
+++ main/motor_control.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <stdio.h>
+#include "driver/mcpwm_prelude.h"
+#include "esp_log.h"
+
+class motor_control
+{
+    private:
+
+    public:
+        void init();
+};
