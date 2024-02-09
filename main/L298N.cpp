7a006f31910b98bb13d13143bb10b6853b35f62f
simonh-nguyen <nguyenh.simon@gmail.com>
2024-02-08 21:07:47 -0800

    Created working MCPWM module

--- main/L298N.cpp
+++ main/L298N.cpp
@@ -2,33 +2,66 @@
 
 #include "L298N.hpp"
 
-#define TIMER_RES   10000000    // 10 MHz 
-#define TIMER_FREQ  30000       // 30 kHz
-#define CMPR_TICKS  20000       
+#define TIMER_RES       10000000    // 10 MHz 
+#define TIMER_FREQ      30000       // 30 kHz
+#define GEN_GPIO        1           // GPIO1
+
+#define TIMER_PERIOD    TIMER_RES / TIMER_FREQ
+
+static const char* TAG = "L298N";
 
 void L298N::init()
 {
-    mcpwm_timer_handle_t timer = NULL;
+    ESP_LOGI(TAG, "Creating timer.");
+    mcpwm_timer_handle_t timer = nullptr;
     mcpwm_timer_config_t timer_config = 
     {
         .group_id = 0,
-        .clk_src = MCPWM_TIMER_CLK_SRC_DEFAULT,
+        .clk_src = MCPWM_TIMER_CLK_SRC_DEFAULT, // 160 MHz
         .resolution_hz = TIMER_RES,
         .count_mode = MCPWM_TIMER_COUNT_MODE_UP,
-        .period_ticks = TIMER_RES / TIMER_FREQ,
+        .period_ticks = TIMER_PERIOD,
     };
+    mcpwm_new_timer(&timer_config, &timer);
 
-    mcpwm_oper_handle_t oper = NULL;
-    mcpwm_operator_config_t oper_config;
-    oper_config.group_id = 0;
+    ESP_LOGI(TAG, "Creating operator.");
+    mcpwm_oper_handle_t oper = nullptr;
+    mcpwm_operator_config_t oper_config =
+    {
+        .group_id = 0,
+    };
+    mcpwm_new_operator(&oper_config, &oper);
 
-    ESP_ERROR_CHECK(mcpwm_new_operator(&oper_config, &oper));
+    ESP_LOGI(TAG, "Connecting timer and operator.");
+    mcpwm_operator_connect_timer(oper, timer);
 
-    mcpwm_cmpr_handle_t cmpr = NULL;
+    ESP_LOGI(TAG, "Creating comparator.");
+    mcpwm_cmpr_handle_t cmpr = nullptr;
     mcpwm_comparator_config_t cmpr_config;
     cmpr_config.flags.update_cmp_on_tez = true;
+    cmpr_config.intr_priority = 0;
+    mcpwm_new_comparator(oper, &cmpr_config, &cmpr);
 
-    ESP_ERROR_CHECK(mcpwm_new_comparator(oper, &cmpr_config, &cmpr));
+    ESP_LOGI(TAG, "Creating generator.");
+    mcpwm_gen_handle_t gen = nullptr;
+    mcpwm_generator_config_t gen_config =
+    {
+        .gen_gpio_num = GEN_GPIO,
+    };
+    mcpwm_new_generator(oper, &gen_config, &gen);
 
-    mcpwm_comparator_set_compare_value(cmpr, CMPR_TICKS);
+    ESP_LOGI(TAG, "Setting initial comparator value.");
+    mcpwm_comparator_set_compare_value(cmpr, 0);
+
+    ESP_LOGI(TAG, "Setting generator's actions on timer and compare events.");
+    mcpwm_generator_set_action_on_timer_event(gen, MCPWM_GEN_TIMER_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, MCPWM_TIMER_EVENT_EMPTY, MCPWM_GEN_ACTION_HIGH));
+    mcpwm_generator_set_action_on_compare_event(gen, MCPWM_GEN_COMPARE_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, cmpr, MCPWM_GEN_ACTION_LOW));
+
+    ESP_LOGI(TAG, "Enabling and starting timer.");
+    mcpwm_timer_enable(timer);
+    mcpwm_timer_start_stop(timer, MCPWM_TIMER_START_NO_STOP);
+
+    mcpwm_comparator_set_compare_value(cmpr, TIMER_PERIOD * 0.30);
+
+    ESP_LOGI(TAG, "Finished MCPWM initialization.");
 }
--- main/main.cpp
+++ main/main.cpp
@@ -5,12 +5,17 @@
 #include "esp_log.h"
 #include "L298N.hpp"
 
+static const char* TAG = "Main";
+
 extern "C" void app_main(void)
 {
+    L298N test;
+    test.init();
+
     while (1)
     {
-        ESP_LOGI("Main", "Test 1");
-        ESP_LOGE("Main", "Test 2");
-        vTaskDelay(500/portTICK_PERIOD_MS);
+        ESP_LOGI(TAG, "Test 1");
+        ESP_LOGE(TAG, "Test 2");
+        vTaskDelay(2000/portTICK_PERIOD_MS);
     }
 }
